d250489ac3d9f2e363cf5dbc9fe8f4c7695384ef
431136-ai <431136@graniteschools.org>
2026-04-14 12:02:30 -0600

    Update TimeManager.java

--- Classes/TimeManager.java
+++ Classes/TimeManager.java
@@ -1,1 +1,25 @@
+public class TimeManager {
+    float maxTime = 100; // 100% of the bar
+    float timeLeft;
+    float drainRate = 0.2f; // How fast time disappears
 
+    public TimeManager() {
+        timeLeft = maxTime;
+    }
+
+    public void update() {
+        if (timeLeft > 0) timeLeft -= drainRate;
+    }
+
+    public void addBonus(float amount) {
+        timeLeft = Math.min(maxTime, timeLeft + amount);
+    }
+
+    public boolean isTimeUp() {
+        return timeLeft <= 0;
+    }
+
+    public void reset() {
+        timeLeft = maxTime;
+    }
+}
