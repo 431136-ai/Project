c1bec251784721558d823599d3aa2b86f06e1945
431136-ai <431136@graniteschools.org>
2026-04-14 11:52:31 -0600

    Add Ball class with jump functionality

--- Classes/Ball.java
+++ Classes/Ball.java
@@ -1,1 +1,11 @@
+public class Ball {
+    double x = 100;
+    double y = 300;
+    int radius = 15;
+    double velocityV = 0;
+    final double jumpForce = -10;
 
+    public void jump() {
+        velocityV = jumpForce;
+    }
+}
