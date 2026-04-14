13671a9b3a5a11a9a5012633aa10af2d33808a9f
431136-ai <431136@graniteschools.org>
2026-04-14 11:54:34 -0600

    Update ScoreManager.java

--- Classes/ScoreManager.java
+++ Classes/ScoreManager.java
@@ -1,1 +1,15 @@
+public class ScoreManager {
+    private int currentScore = 0;
 
+    public void increment() {
+        currentScore++;
+    }
+
+    public void reset() {
+        currentScore = 0;
+    }
+
+    public int getScore() {
+        return currentScore;
+    }
+}
