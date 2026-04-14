72a68328e45687cacc395e327b30df9b9a967db9
431136-ai <431136@graniteschools.org>
2026-04-14 11:53:06 -0600

    Update Hoop.java

--- Classes/Hoop.java
+++ Classes/Hoop.java
@@ -1,1 +1,21 @@
+public class Hoop {
+    double x;
+    double y = 200;
+    int width = 80;
+    int height = 15;
+    double speed = 3;
+    int direction = 1;
+
+    public Hoop(int canvasWidth) {
+        this.x = canvasWidth - 150;
+    }
+
+    public void update(int canvasHeight) {
+        y += speed * direction;
+        // Bounce off top and bottom boundaries
+        if (y > canvasHeight - 150 || y < 50) {
+            direction *= -1;
+        }
+    }
+}
 
