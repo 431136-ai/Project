0a14aebecffea2ba1ef25445b5aa30e29e8d7af5
431136-ai <431136@graniteschools.org>
2026-04-14 11:53:51 -0600

    Update PhysicsEngine.java

--- Classes/PhysicsEngine.java
+++ Classes/PhysicsEngine.java
@@ -1,1 +1,18 @@
+public class PhysicsEngine {
+    double gravity = 0.5;
 
+    public void applyGravity(Ball ball) {
+        ball.velocityV += gravity;
+        ball.y += ball.velocityV;
+    }
+
+    public boolean checkScore(Ball ball, Hoop hoop) {
+        // Checks if ball is within horizontal bounds of hoop 
+        // and passing through vertically while falling
+        return ball.x > hoop.x && 
+               ball.x < hoop.x + hoop.width &&
+               ball.y > hoop.y && 
+               ball.y < hoop.y + hoop.height &&
+               ball.velocityV > 0;
+    }
+}
