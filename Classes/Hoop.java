d4a8633bfb99d73fd8a17c6f14280a4c48d51cf8
431136-ai <431136@graniteschools.org>
2026-04-16 11:47:24 -0600

    Update Hoop class with new properties and methods
    Refactor Hoop class to include dynamic height and toggle position.

--- Classes/Hoop.java
+++ Classes/Hoop.java
@@ -1,21 +1,22 @@
 public class Hoop {
-    double x;
-    double y = 200;
-    int width = 80;
-    int height = 15;
-    double speed = 3;
-    int direction = 1;
+    double x, y;
+    int width = 60;
+    int height = 10;
+    boolean isOnRight = true;
 
-    public Hoop(int canvasWidth) {
-        this.x = canvasWidth - 150;
+    public Hoop(int canvasWidth, int canvasHeight) {
+        this.y = canvasHeight / 2;
+        teleport(canvasWidth);
     }
 
-    public void update(int canvasHeight) {
-        y += speed * direction;
-        // Bounce off top and bottom boundaries
-        if (y > canvasHeight - 150 || y < 50) {
-            direction *= -1;
+    public void teleport(int canvasWidth) {
+        if (isOnRight) {
+            this.x = canvasWidth - 120; // Position on right
+        } else {
+            this.x = 60; // Position on left
         }
+        // Randomize height slightly for variety
+        this.y = 150 + (Math.random() * 200);
+        isOnRight = !isOnRight; // Toggle side for next time
     }
 }
-
