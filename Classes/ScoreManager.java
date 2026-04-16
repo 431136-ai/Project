06b331d01672d01f3ba078b86fb6afdc7fd6f44f
431136-ai <431136@graniteschools.org>
2026-04-16 11:55:38 -0600

    Enhance ScoreManager with scoring streak logic

--- Classes/ScoreManager.java
+++ Classes/ScoreManager.java
@@ -1,15 +1,22 @@
 public class ScoreManager {
     private int currentScore = 0;
+    private int streak = 0;
 
-    public void increment() {
-        currentScore++;
+    public void scoreBasket(boolean isSwish) {
+        if (isSwish) {
+            streak++; // Increase streak modifier
+            currentScore += (1 + streak); // e.g., 1st swish = +2, 2nd swish = +3
+        } else {
+            streak = 0; // Reset streak
+            currentScore += 1; // Standard point
+        }
     }
 
     public void reset() {
         currentScore = 0;
+        streak = 0;
     }
 
-    public int getScore() {
-        return currentScore;
-    }
+    public int getScore() { return currentScore; }
+    public int getStreak() { return streak; }
 }
