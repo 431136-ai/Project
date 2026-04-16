1d0b4f2300cfd6cbec7aed3bed4812b94762ae6a
431136-ai <431136@graniteschools.org>
2026-04-16 11:47:00 -0600

    Update Ball.java

--- Classes/Ball.java
+++ Classes/Ball.java
@@ -2,18 +2,18 @@ public class Ball {
     double x = 100, y = 300;
     int radius = 15;
     double velocityV = 0;
-    double velocityH = 4; // Constant horizontal speed
+    double velocityH = 3; // Constant horizontal speed
     final double jumpForce = -10;
 
     public void jump() {
         velocityV = jumpForce;
     }
 
-    public void updateHorizontal(int canvasWidth) {
-        x += velocityH;
-        // Bounce off left and right walls
-        if (x < 0 || x > canvasWidth - (radius * 2)) {
-            velocityH *= -1;
+    public void wrapAround(int canvasWidth) {
+        if (x > canvasWidth) {
+            x = -radius; // Appear on left
+        } else if (x < -radius) {
+            x = canvasWidth; // Appear on right
         }
     }
 }
