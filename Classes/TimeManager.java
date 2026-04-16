fe873f434938ec66169d744cbc60a433ac4daf9a
431136-ai <431136@graniteschools.org>
2026-04-16 11:56:19 -0600

    Refactor TimeManager for cleaner code structure
    Refactored TimeManager class by removing unnecessary line breaks.

--- Classes/TimeManager.java
+++ Classes/TimeManager.java
@@ -1,25 +1,10 @@
 public class TimeManager {
-    float maxTime = 100; // 100% of the bar
+    float maxTime = 100;
     float timeLeft;
-    float drainRate = 0.2f; // How fast time disappears
+    float drainRate = 0.2f;
 
-    public TimeManager() {
-        timeLeft = maxTime;
-    }
-
-    public void update() {
-        if (timeLeft > 0) timeLeft -= drainRate;
-    }
-
-    public void addBonus(float amount) {
-        timeLeft = Math.min(maxTime, timeLeft + amount);
-    }
-
-    public boolean isTimeUp() {
-        return timeLeft <= 0;
-    }
-
-    public void reset() {
-        timeLeft = maxTime;
-    }
+    public TimeManager() { timeLeft = maxTime; }
+    public void update() { if (timeLeft > 0) timeLeft -= drainRate; }
+    public boolean isTimeUp() { return timeLeft <= 0; }
+    public void reset() { timeLeft = maxTime; }
 }
