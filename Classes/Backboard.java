279bcb48f3867c3c2e7682ee849e9583e2469a84
431136-ai <431136@graniteschools.org>
2026-04-16 11:48:04 -0600

    Implement getBounds method in Backboard class
    Add getBounds method to calculate backboard position based on hoop location.

--- Classes/Backboard.java
+++ Classes/Backboard.java
@@ -1,1 +1,13 @@
+import java.awt.Rectangle;
 
+public class Backboard {
+    int width = 10;
+    int height = 80;
+    
+    public Rectangle getBounds(Hoop hoop) {
+        // If hoop is on the right, backboard is to its right. 
+        // If hoop is on the left, backboard is to its left.
+        int xPos = hoop.isOnRight ? (int)hoop.x - 10 : (int)hoop.x + hoop.width;
+        return new Rectangle(xPos, (int)hoop.y - 40, width, height);
+    }
+}
