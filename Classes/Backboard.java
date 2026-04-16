dfa8d0f0a10cd189a2fb485b8bc9a87354192e76
431136-ai <431136@graniteschools.org>
2026-04-16 11:55:13 -0600

    Update Backboard.java

--- Classes/Backboard.java
+++ Classes/Backboard.java
@@ -2,12 +2,11 @@ import java.awt.Rectangle;
 
 public class Backboard {
     int width = 10;
-    int height = 80;
+    int height = 90;
     
     public Rectangle getBounds(Hoop hoop) {
-        // If hoop is on the right, backboard is to its right. 
-        // If hoop is on the left, backboard is to its left.
-        int xPos = hoop.isOnRight ? (int)hoop.x - 10 : (int)hoop.x + hoop.width;
-        return new Rectangle(xPos, (int)hoop.y - 40, width, height);
+        // Position backboard relative to the hoop's side
+        int xPos = hoop.isOnRight ? (int)hoop.x + hoop.width : (int)hoop.x - width;
+        return new Rectangle(xPos, (int)hoop.y - 60, width, height);
     }
 }
