3a861352c4702fb4d78c8f8da49106b145ba8228
431136-ai <431136@graniteschools.org>
2026-04-21 11:25:32 -0600

    Update Hoop.java

--- Classes/Hoop.java
+++ Classes/Hoop.java
@@ -1,37 +1,19 @@
-import java.awt.Rectangle;
-
 public class Hoop {
     double x, y;
-    int width = 70;
-    int height = 10;
-    int rimWidth = 8;
-    boolean isOnRight = false; // Toggles when teleporting
+    int width = 85; // Slightly wider for a better "feel"
+    boolean isOnRight = false;
 
     public Hoop(int canvasWidth) {
         teleport(canvasWidth);
     }
 
     public void teleport(int canvasWidth) {
-        isOnRight = !isOnRight; // Swap sides
-        
-        // Randomize Y between 150 and 400
-        this.y = 150 + (Math.random() * 250);
-
+        isOnRight = !isOnRight;
+        this.y = 180 + (Math.random() * 220);
         if (isOnRight) {
-            // Teleport anywhere on the RIGHT half (padding from edges)
             this.x = (canvasWidth / 2) + 50 + (Math.random() * ((canvasWidth / 2) - 150));
         } else {
-            // Teleport anywhere on the LEFT half
             this.x = 50 + (Math.random() * ((canvasWidth / 2) - 150));
         }
-    }
-
-    // Physical bounds for bouncing
-    public Rectangle getLeftRim() {
-        return new Rectangle((int)x, (int)y, rimWidth, height);
-    }
-
-    public Rectangle getRightRim() {
-        return new Rectangle((int)(x + width - rimWidth), (int)y, rimWidth, height);
     }
 }
