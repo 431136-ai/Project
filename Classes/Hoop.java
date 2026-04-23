97e86b14717670fb5d523124056276886f453a8a
431136-ai <431136@graniteschools.org>
2026-04-23 12:40:17 -0600

    Update Hoop.java

--- Classes/Hoop.java
+++ Classes/Hoop.java
@@ -1,18 +1,20 @@
 public class Hoop {
     double x, y;
-    int height = 100; // The vertical "opening" size
-    double speed = 3.8; // How fast the world moves toward you
+    int width = 60; // Horizontal basket
+    double speed = 4.0; // The scrolling speed
+    boolean isScored = false; 
 
-    public Hoop(int canvasWidth) {
-        reset(canvasWidth);
+    public Hoop(int canvasWidth) { 
+        reset(canvasWidth); 
     }
 
     public void reset(int canvasWidth) {
-        this.x = canvasWidth + 50; // Spawns off-screen to the right
-        this.y = 100 + (Math.random() * 300); // Random vertical position
+        this.x = canvasWidth + 50; // Spawn off-screen to the right
+        this.y = 150 + (Math.random() * 250); // Random height
+        this.isScored = false;
     }
 
     public void move() {
-        this.x -= speed; // Moves the hoop toward the left
+        this.x -= speed; // Move towards the left
     }
 }
