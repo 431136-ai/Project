5193a8c04929fbddeae6b9484d99b96f1cfa3f43
431136-ai <431136@graniteschools.org>
2026-04-23 11:45:14 -0600

    Refactor TimeManager to simplify time management

--- Classes/TimeManager.java
+++ Classes/TimeManager.java
@@ -1,10 +1,10 @@
 public class TimeManager {
-    float maxTime = 100;
-    float timeLeft;
-    float drainRate = 0.2f;
+    float timeLeft = 100, drainRate = 0.22f;
 
-    public TimeManager() { timeLeft = maxTime; }
-    public void update() { if (timeLeft > 0) timeLeft -= drainRate; }
+    public void update() { 
+        if (timeLeft > 0) timeLeft -= drainRate; 
+    }
+    
     public boolean isTimeUp() { return timeLeft <= 0; }
-    public void reset() { timeLeft = maxTime; }
+    public void reset() { timeLeft = 100; }
 }
