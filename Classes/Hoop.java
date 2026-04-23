b97fa7128e92f64d1db22045a372b2ff44bb007f
431136-ai <431136@graniteschools.org>
2026-04-23 11:44:23 -0600

    Refactor Hoop class constructor and teleport method

--- Classes/Hoop.java
+++ Classes/Hoop.java
@@ -1,21 +1,17 @@
 public class Hoop {
     double x, y;
-    int width = 85; 
-    boolean isOnRight = false; // Alternates properly
+    int width = 45; // Pro size
+    boolean isOnRight = false; 
 
-    public Hoop(int canvasWidth) {
-        teleport(canvasWidth);
+    public Hoop(int w) { 
+        teleport(w); 
     }
 
-    public void teleport(int canvasWidth) {
-        isOnRight = !isOnRight; 
-        this.y = 180 + (Math.random() * 220); // Keep Y random for vertical challenge
-        
-        // Strictly left or right side placement
-        if (isOnRight) {
-            this.x = canvasWidth - width - 60; // Far right
-        } else {
-            this.x = 60; // Far left
-        }
+    public void teleport(int w) {
+        isOnRight = !isOnRight; // Strict alternation
+        // Random height between 180 and 400
+        this.y = 180 + (Math.random() * 220); 
+        // Position it on the far left or far right
+        this.x = isOnRight ? w - width - 80 : 80;
     }
 }
