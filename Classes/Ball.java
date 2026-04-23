ec40073a626daf2880b7b7d43be17ee7e71fd519
431136-ai <431136@graniteschools.org>
2026-04-23 12:40:04 -0600

    Update Ball.java

--- Classes/Ball.java
+++ Classes/Ball.java
@@ -1,16 +1,11 @@
 public class Ball {
-    double x = 400, y = 300; 
-    int radius = 14; 
-    double velocityV = 0, velocityH = 4.0;
+    double x = 250, y = 300; // Anchored to the left side
+    int radius = 14;
+    double velocityV = 0, velocityH = 0;
     final double jumpForce = -9.5;
     boolean hitRim = false; 
 
     public void jump() { 
         velocityV = jumpForce; 
-    }
-
-    public void wrapAround(int w) {
-        if (x > w) x = -radius; 
-        else if (x < -radius) x = w;
     }
 }
