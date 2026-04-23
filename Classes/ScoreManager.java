1aebb63f8b5a7c827c1651fc0e17cbec663c00df
431136-ai <431136@graniteschools.org>
2026-04-23 11:44:59 -0600

    Update ScoreManager.java

--- Classes/ScoreManager.java
+++ Classes/ScoreManager.java
@@ -1,22 +1,17 @@
 public class ScoreManager {
-    private int currentScore = 0;
-    private int streak = 0;
+    private int score = 0, streak = 0;
 
-    public void scoreBasket(boolean isSwish) {
-        if (isSwish) {
-            streak++; // Increase streak modifier
-            currentScore += (1 + streak); // e.g., 1st swish = +2, 2nd swish = +3
-        } else {
-            streak = 0; // Reset streak
-            currentScore += 1; // Standard point
+    public void scoreBasket(boolean swish) {
+        if (swish) { 
+            streak++; 
+            score += (1 + streak); 
+        } else { 
+            streak = 0; 
+            score += 1; 
         }
     }
 
-    public void reset() {
-        currentScore = 0;
-        streak = 0;
-    }
-
-    public int getScore() { return currentScore; }
+    public void reset() { score = 0; streak = 0; }
+    public int getScore() { return score; }
     public int getStreak() { return streak; }
 }
