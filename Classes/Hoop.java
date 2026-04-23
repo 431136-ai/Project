6e4fffd1e8215d882dedbcecc998c860907ca82b
431136-ai <431136@graniteschools.org>
2026-04-23 12:43:42 -0600

    Update Hoop.java

--- Classes/Hoop.java
+++ Classes/Hoop.java
@@ -1,20 +1,20 @@
 public class Hoop {
     double x, y;
-    int width = 60; // Horizontal basket
-    double speed = 4.0; // The scrolling speed
-    boolean isScored = false; 
+    int width = 60; 
+    boolean isOnRight = false; 
 
     public Hoop(int canvasWidth) { 
         reset(canvasWidth); 
     }
 
     public void reset(int canvasWidth) {
-        this.x = canvasWidth + 50; // Spawn off-screen to the right
-        this.y = 150 + (Math.random() * 250); // Random height
-        this.isScored = false;
-    }
-
-    public void move() {
-        this.x -= speed; // Move towards the left
+        // Swap sides
+        isOnRight = !isOnRight; 
+        
+        // Random Y axis height (between 150 and 400)
+        this.y = 150 + (Math.random() * 250); 
+        
+        // Position on the left or right side, facing inward
+        this.x = isOnRight ? canvasWidth - width - 30 : 30;
     }
 }
