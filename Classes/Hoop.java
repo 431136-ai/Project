0e48b64daf5cb37be193586fa1871e9342d00abb
431136-ai <431136@graniteschools.org>
2026-04-24 11:51:24 -0600

    Update Hoop.java

--- Classes/Hoop.java
+++ Classes/Hoop.java
@@ -8,13 +8,13 @@ public class Hoop {
     }
 
     public void reset(int canvasWidth) {
-        // Swap sides
         isOnRight = !isOnRight; 
-        
-        // Random Y axis height (between 150 and 400)
         this.y = 150 + (Math.random() * 250); 
-        
-        // Position on the left or right side, facing inward
+        updateResize(canvasWidth);
+    }
+
+    // Keeps the hoop on the correct wall if the window goes fullscreen
+    public void updateResize(int canvasWidth) {
         this.x = isOnRight ? canvasWidth - width - 30 : 30;
     }
 }
