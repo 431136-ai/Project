df06cd1e00c387bc760ea3db358075f482b08902
431136-ai <431136@graniteschools.org>
2026-04-30 12:18:21 -0600

    Update Hoop.java

--- Classes/Hoop.java
+++ Classes/Hoop.java
@@ -1,19 +1,20 @@
 public class Hoop {
     double x, y;
-    int width = 70; 
-    int thickness = 8;
-    int netHeight = 60;
+    int width = 75; 
+    int thickness = 5; // Reduced from 8 for a sleeker look
+    int netHeight = 70;
     boolean isOnRight = false; 
 
     public Hoop(int canvasWidth) { reset(canvasWidth); }
 
     public void reset(int canvasWidth) {
         isOnRight = !isOnRight; 
-        this.y = 150 + (Math.random() * 250); 
+        this.y = 100 + (Math.random() * 300); 
         updateResize(canvasWidth);
     }
 
     public void updateResize(int canvasWidth) {
-        this.x = isOnRight ? canvasWidth - width - 50 : 50;
+        // Added a bit more padding from the wall for realism
+        this.x = isOnRight ? canvasWidth - width - 60 : 60;
     }
 }
